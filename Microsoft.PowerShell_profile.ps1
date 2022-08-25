7a3d484ce5961bd4c1bde8f1282d926908716eec
Raíssa Regis <48386341+raissarcl@users.noreply.github.com>
2022-08-25 15:58:25 -0300

    added profile file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+function prompt {
+
+  $p = split-path -leaf -path (get-Location)
+  $q = split-path -parent -path (get-Location)
+  $r = split-path -qualifier -path (get-Location)
+
+  if($q) {
+    $s = split-path -leaf -path ($q)
+  }
+
+  if ($q -eq $s) {
+    "PS $q$p> "
+  } elseif (-not $q) {
+    "PS $r> "
+  } else {
+      "PS $s/$p> "
+  }
+}
